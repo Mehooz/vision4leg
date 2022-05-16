b630d09eb45ac8c363c4f6eb44168eab5b208240
lqy0057 <785878978@qq.com>
2022-05-16 10:37:08 +0800

    close render

--- starter/ppo_state.py
+++ starter/ppo_state.py
@@ -21,7 +21,7 @@ import torch
 
 args = get_args()
 params = get_params(args.config)
-params["env"]["env_build"]["enable_rendering"] = True
+# params["env"]["env_build"]["enable_rendering"] = True
 
 
 def experiment(args):
